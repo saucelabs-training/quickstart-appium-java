283c1fbbe1cd367d62f4d6f0e1a3fe06de4eeafe
Eyal Yovel <eyalyovel@gmail.com>
2023-05-12 22:53:43 +0100

    Update Config.java

--- src/test/java/tests/Config.java
+++ src/test/java/tests/Config.java
@@ -1,7 +1,7 @@
 package tests;
 
 public class Config {
-    public static final String region = System.getProperty("region", "eu");
+    public static final String region = System.getProperty("region", "us");
     public static final String SAUCE_EU_URL = "https://ondemand.eu-central-1.saucelabs.com:443/wd/hub";
     public static final String SAUCE_US_URL = "https://ondemand.us-west-1.saucelabs.com:443/wd/hub";
 
